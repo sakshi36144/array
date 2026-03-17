9d61b990e249a4383f92a1c752e59a498904b550
sakshi36144 <pals36144@gmail.com>
2026-03-17 12:54:10 +0530

    print the negative value

---
+++ Array/A2.java
@@ -0,0 +1,22 @@
+// print negative values in an array 
+package Array;
+
+import java.util.Scanner;
+
+public class A2 {
+    public static void main(String[] args){
+    Scanner sc  = new Scanner(System.in);
+     int n = sc.nextInt();
+     int[] arr = new int[n];
+    
+     for(int i = 0; i<n; i++){
+        arr[i] = sc.nextInt();
+         }
+       
+     for (int i =0; i<n; i++){
+        if(arr[i]<0){
+            System.out.println( arr[i]+ " ") ;     }
+     }
+     sc.close();
+}
+}
