f5efd43b4843b56999d72e7d227d2301184349d7
sakshi36144 <pals36144@gmail.com>
2026-04-10 23:07:26 +0530

    this is not solving problem

---
+++ loop_p/pattern/Q12.java
@@ -0,0 +1,19 @@
+package loop_p.pattern;
+
+public class Q12 {
+    public static void main(String[] args){
+        for(int i=4;i>=0;i++){
+            for( int j=0;j<=8;j++){
+               
+              if(){
+                  System.out.print("*");}
+ else{
+                System.out.print(" ");
+            }}
+            System.out.println();   break;
+    }
+           
+        }
+        
+     }
+
