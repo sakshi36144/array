19ded84e23f03c5104ec0bc632833596a3d3cf48
sakshi36144 <pals36144@gmail.com>
2026-04-10 23:04:50 +0530

    this is new pattern question

---
+++ loop_p/pattern/Q11.java
@@ -0,0 +1,17 @@
+package loop_p.pattern;
+
+public class Q11{
+    public static void main(String[] args){
+        for(int i=0;i<=4;i++){
+            for( int j=0;j<=4;j++){
+                if(i==2&&j==1||i==1&&j==2||i==2&&j==3||i==3&&j==2){
+                System.out.print(" ");}
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+}
