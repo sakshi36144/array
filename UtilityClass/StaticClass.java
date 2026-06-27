c1fd3f72f8bbc1cd9b2e5f45bde98161b53526ec
sakshi36144 <pals36144@gmail.com>
2026-06-27 15:40:47 +0530

    utility class

---
+++ UtilityClass/StaticClass.java
@@ -0,0 +1,50 @@
+package UtilityClass;
+// Non -Static Nested Class ---->Topic
+
+class outer{
+   String name="vinay";
+   static String compName ="HCL";
+     
+   public void instanceMethod(){
+    System.out.println(name);
+    System.out.println(compName);
+   }
+
+   public static void staticMethod(){
+    // System.out.println(name);
+    System.out.println(compName);
+   }
+
+
+class Inner{
+    int id= 33;
+    static float salary= 5555f;
+
+    public void instanceMethod2(){
+    System.out.println(name);
+    System.out.println(compName);
+    System.out.println(id);
+    System.out.println(salary);
+   }
+
+   public static void staticMethod2(){
+    // System.out.println(name);
+    System.out.println(compName);
+    // System.out.println(id);
+    System.out.println(salary);
+
+   }
+    
+}
+}
+public class StaticClass{
+    public static void main(String[] args){
+        System.out.println(outer.compName);
+        outer.staticMethod();
+
+
+        outer Outer=new outer();
+         Outer.instanceMethod();
+        outer.Inner inner=Outer.new Inner();
+    }
+}
